5a7b3f8f9dd12527177f70fde5b285d840c66dd7
Tomasz Stanislawski <toms@onspecta.com>
2021-03-26 00:47:22 +0100

    reorganize the header

--- dynarr.h
+++ dynarr.h
@@ -4,38 +4,15 @@
 #include <stddef.h>
 #include <stdlib.h>
 
-/* typical usage:
- *
- * double *arr = DA_INIT;
- * da_push(&arr, 1);
- * da_push(&arr, 2);
- * da_push(&arr, 3);
- * for (size_t i = 0; i < da_size(arr); ++i)
- *    printf("%ld ", arr[i]);
- * da_free(&arr);
- *
+/*
+ *   PRIVATE STUFF
  */
-
-#define da_push(ap, elem) \
-  (da_grow((ap), 1) == 0 ? ((*(ap))[da_size(ap) - 1] = (elem), 0) : -1)
-
-#define da_resize(a,size)  da_resize_((a), (size), sizeof **(a))
-#define da_reserve(a,size) da_reserve_((a), (size), sizeof **(a))
-#define da_grow(a,diff)    da_grow_((a), (diff), sizeof *(a))
-
-// todo:
-//#define da_create(TYPE,SIZE,VAL) da_create(&(type){VAL}
-//#define da_pop(arrp)
-//#define da_put()
-//#define da_get()
-
-#define DA_ALIGN _Alignof(max_align_t)
 
 struct da_header_ {
 	size_t size;
 	size_t caps;
 	// if da_header_ is max aligned then `data` is as well
-	_Alignas(DA_ALIGN) char data[];
+	_Alignas(_Alignof(max_align_t)) char data[];
 };
 
 static inline struct da_header_ *da_header_(char* data) {
@@ -45,25 +22,6 @@ static inline struct da_header_ *da_header_(char* data) {
 }
 
 static struct da_header_ da_dummy_;
-
-#define DA_INIT ((void*)da_dummy_.data)
-
-static inline void *da_init(void) {
-  return DA_INIT;
-}
-
-static inline size_t da_size(void *pdata) {
-	return da_header_(*(void**)pdata)->size;
-}
-
-static inline size_t da_caps(void *pdata) {
-	return da_header_(*(void**)pdata)->caps;
-}
-
-static inline void da_free(void *pdata) {
-	struct da_header_ *hdr = da_header_(*(void**)pdata);
-	if (hdr->caps) free(hdr);
-}
 
 static inline struct da_header_ *da_realloc_(struct da_header_ *hdr, size_t caps, size_t elemsize) {
 	size_t bytes = sizeof (struct da_header_) + caps * elemsize;
@@ -90,7 +48,7 @@ static inline int da_grow_(void *pdata, size_t diff, size_t elemsize) {
 	struct da_header_ *hdr = da_header_(*(void**)pdata);
 	size_t newsize = hdr->size + diff;
 	if (newsize > hdr->caps) {
-		size_t caps = (5 * newsize) / 4 + 1;
+		size_t caps = (5 * newsize + 3) / 4;
 		hdr = da_realloc_(hdr, caps, elemsize);
 		if (!hdr) return -1;
 		*(void**)pdata = hdr->data;
@@ -107,5 +65,53 @@ static inline int da_resize_(void *pdata, size_t size, size_t elemsize) {
 	*(void**)pdata = hdr->data;
 	return 0;
 }
+
+/*
+ *   PUBLIC API
+ */
+
+/* typical usage:
+ *
+ * double *arr = DA_INIT;
+ * da_push(&arr, 1);
+ * da_push(&arr, 2);
+ * da_push(&arr, 3);
+ * for (size_t i = 0; i < da_size(arr); ++i)
+ *    printf("%ld ", arr[i]);
+ * da_free(&arr);
+ *
+ */
+
+#define DA_INIT ((void*)da_dummy_.data)
+
+static inline void *da_init(void) {
+  return DA_INIT;
+}
+
+#define da_resize(a,size)  da_resize_((a), (size), sizeof **(a))
+#define da_reserve(a,size) da_reserve_((a), (size), sizeof **(a))
+#define da_grow(a,diff)    da_grow_((a), (diff), sizeof *(a))
+
+static inline size_t da_size(void *pdata) {
+	return da_header_(*(void**)pdata)->size;
+}
+
+static inline size_t da_caps(void *pdata) {
+	return da_header_(*(void**)pdata)->caps;
+}
+
+static inline void da_free(void *pdata) {
+	struct da_header_ *hdr = da_header_(*(void**)pdata);
+	if (hdr->caps) free(hdr);
+}
+
+#define da_push(ap, elem) \
+  (da_grow((ap), 1) == 0 ? ((*(ap))[da_size(ap) - 1] = (elem), 0) : -1)
+
+// todo:
+//#define da_create(TYPE,SIZE,VAL) da_create(&(type){VAL}
+//#define da_pop(arrp)
+//#define da_put()
+//#define da_get()
 
 #endif
